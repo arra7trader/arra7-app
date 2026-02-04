0526dbab2ff88013010b0a674c9cdb5f1514fb75
Arratujuh Kreasi <arratujuhkreasi@example.com>
2026-02-05 04:39:04 +0700

    chore: revert to WebView mode as home screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
-import 'screens/dashboard_screen.dart';
+import 'screens/webview_screen.dart';
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
@@ -23,7 +23,7 @@ void main() {
     title: 'ARRA7',
     debugShowCheckedModeBanner: false,
     color: Colors.black,
-    home: DashboardScreen(),
+    home: WebViewScreen(),
   ));
 }
 
